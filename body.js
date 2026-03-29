be4d93dd4f13a48a36a593b1326878dd854d7082
GloriaMirian <113205870+GloriaMirian@users.noreply.github.com>
2026-03-28 22:14:59 -0300

    Update body.js

--- body.js
+++ body.js
@@ -1,20 +1,44 @@
+
+    const btn = document.createElement("button");
+    btn.textContent = "Excluir";
+
+    btn.onclick = () => deleteUser(user.id);
+
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
+}
+
 async function addUser() {
-  await fetch("https://jsonplaceholder.typicode.com/users", {
+  const input = document.getElementById("nameInput");
+
+  if (!input.value) return;
+
+  const response = await fetch(API, {
     method: "POST",
     body: JSON.stringify({
-      name: "Novo Usuário"
+      name: input.value
     }),
     headers: {
       "Content-Type": "application/json"
     }
   });
 
-  alert("Usuário enviado!");
+  const newUser = await response.json();
 
+  users.push(newUser); // adiciona na tela
+  input.value = "";
+  render();
+}
+
+// ❌ Deletar usuário (API + tela)
 async function deleteUser(id) {
-  await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+  await fetch(`${API}/${id}`, {
     method: "DELETE"
   });
 
-  alert("Usuário deletado!");
+  users = users.filter(user => user.id !== id);
+  render();
 }
+
+loadUsers();
